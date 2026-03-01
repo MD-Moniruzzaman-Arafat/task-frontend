8388bf3e1d2f7c6c79cfca335e8fc877435b363b
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-03-01 13:59:12 +0600

    companis section component design

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,24 @@
+import img1 from '../../assets/image/companies/amd-logo-1.png';
+import img2 from '../../assets/image/companies/intel-3.png';
+import img3 from '../../assets/image/companies/talkit 1.png';
+import img4 from '../../assets/image/companies/tesla-9 1.png';
+import img5 from '../../assets/image/companies/vodafone-2017-logo.png';
+
+export default function Companies() {
+  return (
+    <>
+      <div className="container mx-auto my-10">
+        <p>
+          <small className="text-[#BCBDC1]">Companies we helped grow</small>
+        </p>
+        <div className=" flex flex-wrap justify-between items-center gap-5 py-10">
+          <img src={img5} alt="Vodafone Logo" />
+          <img src={img2} alt="Intel Logo" />
+          <img src={img4} alt="Tesla Logo" />
+          <img src={img1} alt="AMD Logo" />
+          <img src={img3} alt="Talkit Logo" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Banner from '../components/Banner/Banner';
+import Companies from '../components/Companies/Companies';
 
 export default function HomePage() {
   return (
     <>
       <Banner />
+      <Companies />
     </>
   );
 }
